1b940bf87de7b6b2c301600ae3615a9cd675d47e
wang <729339468@qq.com>
2022-02-19 19:40:51 +0800

    实现客户端读取到github，以及动态更新数据的手动刷新（需要发送post请求，无需重启）

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope // 开启手动刷新 发送post http://localhost:3355/actuator/refresh
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
