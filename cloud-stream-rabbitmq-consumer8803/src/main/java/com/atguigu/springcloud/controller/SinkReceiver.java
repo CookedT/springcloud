23610787b08195faaa0fb058f0322ed4456ddbf3
wang <729339468@qq.com>
2022-02-20 16:42:20 +0800

    依照8802 clone出8803

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springcloud/controller/SinkReceiver.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@EnableBinding(Sink.class)
+@Slf4j
+public class SinkReceiver {
+
+    @StreamListener(Sink.INPUT)
+    public void receive(Object payload) {
+        log.info("Received: " + payload);
+    }
+}
